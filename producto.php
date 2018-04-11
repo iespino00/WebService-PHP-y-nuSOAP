d5470ddb05ca2411295535805773eb8364cc8e01
IESPINO <iespino69@gmail.com>
2018-04-11 11:08:28 -0500

    Upload

---
+++ producto.php
@@ -0,0 +1,33 @@
+<?php
+require_once "lib_soap/nusoap.php";
+      
+function getProd($categoria) 
+    {
+        if ($categoria == "libros") {
+            return join(",", array(
+                "El señor de los anillos",
+                "Los límites de la Fundación",
+                "The Rails Way"));
+        }
+        else {
+            return "No hay productos de esta categoria";
+        }
+    }
+      
+    $server = new soap_server();
+    $server->configureWSDL("PRODUCTOS", "urn:producto");
+      
+    $server->register("getProd",
+        array("categoria" => "xsd:string"),
+        array("return" => "xsd:string"),
+        "urn:producto",
+        "urn:producto#getProd",
+        "rpc",
+        "encoded",
+        "Nos da una lista de productos de cada categoría");
+      
+    //$server->service($post = file_get_contents('php://input'));
+if ( !isset( $HTTP_RAW_POST_DATA ) ) $HTTP_RAW_POST_DATA =file_get_contents( 'php://input' );
+
+$server->service($HTTP_RAW_POST_DATA);
+ ?> 
